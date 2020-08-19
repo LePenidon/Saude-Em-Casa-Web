e8a67df6ff40c77698251afc14f99362103c2cd7
Gabriel Penido <38563539+LePenidon@users.noreply.github.com>
2020-08-19 16:21:35 -0300

    Arrumar Firebase
    mudamos um tanto de coisa

--- public/js/config.js
+++ public/js/config.js
@@ -1,10 +1,13 @@
 //FIREBASSE INICIALIZATION
 var config = {
-    apiKey: "AIzaSyDcO8_VIP2P2ztqncbgGhLA_mJpuoaC9vo",
-    authDomain: "recicleplus.firebaseapp.com",
-    databaseURL: "https://recicleplus.firebaseio.com",
-    projectId: "recicleplus",
-    storageBucket: "recicleplus.appspot.com",
+    apiKey: "AIzaSyDu6EwbJmI_shg85wfs6Am88KjANjs3rns",
+    authDomain: "coronapp-tcc.firebaseapp.com",
+    databaseURL: "https://coronapp-tcc.firebaseio.com",
+    projectId: "coronapp-tcc",
+    storageBucket: "coronapp-tcc.appspot.com",
+    messagingSenderId: "11598567402",
+    appId: "1:11598567402:web:5f3a398f0dcceca6338493",
+    measurementId: "G-S1FF9N0ZF9"
 };
 firebase.initializeApp(config);
 var scnd = firebase.initializeApp(config, "scnd");
